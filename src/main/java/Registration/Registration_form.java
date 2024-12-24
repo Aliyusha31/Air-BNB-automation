6216a1d8e10613ac7c4b34e40d92a2b8a3599b5d
Aliya <aliyaizabekova4@gmail.com>
2024-12-25 00:26:32 +0600

    Registration form

--- src/main/java/Registration/Registration_form.java
+++ src/main/java/Registration/Registration_form.java
@@ -3,5 +3,5 @@ package Registration;
 public class Registration_form {
     public static void main(String[] args) {
         System.out.println("Welcome to Air BNB!");
-    }
+    }//not working
 }
